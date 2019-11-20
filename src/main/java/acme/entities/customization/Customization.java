7cafedea1d2a215314c59ba2a7a6c0f9d6573928
brapercob <brayanpercob@gmail.com>
2019-11-20 17:14:39 +0100

    Validaciones de customización y ofertas

--- src/main/java/acme/entities/customization/Customization.java
+++ src/main/java/acme/entities/customization/Customization.java
@@ -1,17 +1,9 @@
 
 package acme.entities.customization;
 
-import java.util.List;
-
-import javax.persistence.CollectionTable;
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-
-import org.hibernate.annotations.LazyCollection;
-import org.hibernate.annotations.LazyCollectionOption;
 
 import acme.framework.entities.DomainEntity;
 import lombok.Getter;
@@ -24,11 +16,8 @@ public class Customization extends DomainEntity {
 
 	private static final long	serialVersionUID	= 1L;
 
-	@ElementCollection
-	@CollectionTable(name = "customization_spam_words", joinColumns = @JoinColumn(name = "customization_id"))
-	@Column(name = "spam_words")
-	@LazyCollection(LazyCollectionOption.FALSE)
-	private List<String>		spamWords;
+	@NotBlank
+	private String				spamWords;
 
 	@NotNull
 	private Double				threshold;
